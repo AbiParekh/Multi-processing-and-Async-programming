2c48a5c4bf9906cff1e492cf31ee4a981e07c0e9
Abhishek Parekh <abhishekparekh25@yahoo.ca>
2023-09-13 19:01:25 -0400

    added api.py and teamDetails.py

---
+++ api.py
@@ -0,0 +1,60 @@
+# API class
+import json
+import requests
+from teamDetails import TeamStatistics
+
+
+class API:
+    def __init__(self):
+        self.base_url = "https://sports.snoozle.net/search/nfl/searchHandler?"
+        self.file_type = "inline"
+        self.stat_type = "teamStats"
+        self.season = 2020
+        self.http_client = requests.Session()
+
+    def get_team_statistics(self):
+        team_stats_list = []
+
+        for team_name in range(1, 33):  # iterate over the teams from 1-32
+            url = f"{self.base_url}fileType={self.file_type}&statType={self.stat_type}&season={self.season}" \
+                  f"&teamName={team_name}"
+            response = self.http_client.get(url)
+            response_json = response.json()
+            print("Request URL:", url)
+            print("Response Status Code:", response.status_code)
+            print("Response Text:", response.text)  # Print the response content
+            print("JSON Response:", response_json)
+            print()
+
+            # Save the JSON response to a file
+            with open(f"team_stats_{team_name}.json", "w") as json_file:
+                json.dump(response_json, json_file, indent=4)
+
+            if 'matchUpStats' in response_json:
+                match_up_stats = response_json['matchUpStats']
+
+                for match_stats in match_up_stats:
+                    vis_stats = match_stats.get('visStats', {})
+                    home_stats = match_stats.get('homeStats', {})
+
+                    if 'teamName' in vis_stats:
+                        team_stats_list.append(
+                            TeamStatistics(
+                                vis_stats['teamName'],
+                                vis_stats.get('teamNumber', ''),
+                                vis_stats.get('teamCode', ''),
+                                vis_stats.get('score')
+                            )
+                        )
+
+                    if 'teamName' in home_stats:
+                        team_stats_list.append(
+                            TeamStatistics(
+                                home_stats['teamName'],
+                                home_stats.get('teamNumber', ''),
+                                home_stats.get('teamCode', ''),
+                                home_stats.get('score')
+                            )
+                        )
+
+        return team_stats_list
--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import threading
+from api import API
 
 app = Flask(__name__)
+CORS(app)
 
+team_stats_cache = []
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/team_statistics', methods=['GET'])
+def team_statistics():
+    global team_stats_cache
+    return jsonify(team_stats_cache), 200
 
+def communication_thread_function():
+    api_call = API()
+    global team_stats_cache
+    team_stats_cache = [team_stat.add_to_tuple() for team_stat in api_call.get_team_statistics()]
 
-if __name__ == '__main__':
-    app.run()
+def presentation_thread_function():
+    while not team_stats_cache:
+        print("Presentation thread: Waiting for data from the communication thread...")
+        threading.Event().wait(timeout=1)
+    print("Presentation thread: Data ready for presentation!")
+
+if __name__ == "__main__":
+    # Start the threads
+    threading.Thread(target=communication_thread_function).start()
+    threading.Thread(target=presentation_thread_function).start()
+
+    # Start the Flask app
+    app.run(debug=True)
